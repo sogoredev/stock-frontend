16a48958a34f0ef7e66ec4e7e4c15f0247fc29bf
sogoredev <sogoredev@gmail.com>
2024-08-20 20:02:02 +0000

    testt

--- src/main/java/com/gds/Gestion/de/stock/GestionDeStockApplication.java
+++ src/main/java/com/gds/Gestion/de/stock/GestionDeStockApplication.java
@@ -3,9 +3,8 @@ package com.gds.Gestion.de.stock;
 
 import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 @AllArgsConstructor
 public class GestionDeStockApplication{
 	
